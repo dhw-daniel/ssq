a01b0923818580ecaa6bdac2f6f7101dfbc1d5aa
daniel <506682730@qq.com>
2018-11-06 11:49:47 +0800

    配置跨域

--- public/index.php
+++ public/index.php
@@ -12,6 +12,11 @@
 // [ 应用入口文件 ]
 namespace think;
 
+header('Access-Control-Allow-Origin:*');
+// 响应类型
+header('Access-Control-Allow-Methods:*');
+// 响应头设置
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
 // 加载基础文件
 require __DIR__ . '/../thinkphp/base.php';
 
